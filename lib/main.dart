1b3916ce00d143549e4d92a7ab3d14b14d5998b1
unknown <Eze-jordan>
2024-12-13 13:41:10 +0100

    MAJ

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:foot_app/ecran/home_page.dart';
+import 'package:foot_app/ecran/welcome_page';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const HomePage(),
+      home: const WelcomePage(),
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: 'Roboto',
